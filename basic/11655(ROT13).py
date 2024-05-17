5935d61475a611b71c4fe613be22100a3d9d77f8
ivk <leorivk@gmail.com>
2024-05-18 08:46:37 +0900

    11655(ROT13)

---
+++ basic/11655(ROT13).py
@@ -0,0 +1,17 @@
+s=input()
+answer=''
+for i in s:
+    if 'a'<=i<='z':
+        i = ord(i) + 13
+        if i > 122:
+            i -= 26
+        answer += chr(i)
+    elif 'A'<=i<='Z':
+        i = ord(i) + 13
+        if i > 90:
+            i -= 26
+        answer += chr(i)
+    else:
+        answer+=i
+
+print(answer)
