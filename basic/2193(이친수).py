f60b4c75d0700e48fcebc1954d9916dc864cc080
ivk <leorivk@gmail.com>
2024-06-04 11:33:41 +0900

    2193(이친수)

---
+++ basic/2193(이친수).py
@@ -0,0 +1,12 @@
+n = int(input())
+
+d = [[0, 0] for _ in range(91)]
+
+d[1][1] = 1
+
+for i in range(2, n+1):
+    d[i][0] = d[i-1][0] + d[i-1][1]
+    d[i][1] = d[i-1][0]
+    
+    
+print(d[n][0] + d[n][1])
