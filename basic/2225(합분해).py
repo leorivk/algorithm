b882c70a7613cef30f10eddd26ae72fd1dfca985
ivk <leorivk@gmail.com>
2024-07-08 08:36:14 +0900

    2225(합분해)

---
+++ basic/2225(합분해).py
@@ -0,0 +1,9 @@
+n, k = map(int, input().split())
+d = [[0] * (k+1) for _ in range(n+1)]
+
+d[0][0] = 1
+
+for i in range(n+1):
+    for j in range(1, k+1):
+        d[i][j] = d[i][j-1] + d[i-1][j]
+print(d[n][k] % 1000000000)
